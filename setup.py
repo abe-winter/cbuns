75c65b72850f2a5096d14a68c4985c362d46961d
Abe Winter <abe-winter@users.noreply.github.com>
2015-07-18 00:13:02 -0400

    skeleton

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+  name='cbuns',
+  version='0.0.1',
+  description='a package manager for C',
+  classifiers=[
+  ],
+  keywords=['c','compiler','transpiler','package management'],
+  author='Abe Winter',
+  author_email='abe-winter@users.noreply.github.com',
+  url='https://github.com/abe-winter/cbuns',
+  license='MIT',
+  entry_points={
+    'console_scripts':[
+    ],
+  },
+  packages=setuptools.find_packages(),
+  install_requires=[
+    'pycparser',
+  ],
+)
