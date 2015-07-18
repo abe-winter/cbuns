e7fe9288913a72b685b2f86a55afeecfd9d7e121
Abe Winter <abe-winter@users.noreply.github.com>
2015-07-18 18:29:39 -0400

    [transformation] new entry points -- but these should be in separate files

--- cbuns/steps/transform.py
+++ cbuns/steps/transform.py
@@ -85,3 +85,23 @@ def pretralp():
   tralper.process(open(args.path).read())
   print tralper.summary()
 
+def imex():
+  parser = argparse.ArgumentParser(description="imex is the 'import exports' process for cbuns. this analyzes the package and the used symbols and prints the extern & typedefs that would be inserted in place of @import")
+  parser.add_argument('package', help='path to package.json')
+  parser.add_argument('symbols', help='comma-separated list of symbols; e.g. "pkg.subpkg.function_name,pkg.function2" (leave out the quotes, but pkg prefix necessary)')
+  args = parser.parse_args()
+
+  symbols = [tuple(sym.split('.')) for sym in args.symbols.split(',')]
+  raise NotImplementedError
+
+def transform():
+  parser = argparse.ArgumentParser(description='transform a C file with @imports to straight C')
+  parser.add_argument('package', help='path to package.json (necessary because it defines exports)')
+  parser.add_argument('path', help='path of input file')
+  parser.add_argument('-o', '--out', help='path to output. if a folder, use same filename as input. if not given, use stdout')
+  args = parser.parse_args()
+
+  # 1. tralp the thing
+  # 2. get exports
+  # 3. write with interpolation -- we're going to want line & slice of each substitution from symbols, and line for import stmts
+  raise NotImplementedError
