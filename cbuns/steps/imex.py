15e5193f1dc111b5faf51d56a4c32de103cfb962
Abe Winter <abe-winter@users.noreply.github.com>
2015-07-19 19:05:29 -0400

    [imex] working

--- cbuns/steps/imex.py
+++ cbuns/steps/imex.py
@@ -2,11 +2,38 @@
 
 import argparse, os, json, pycparser, collections
 from . import depgraph
+from .. import pkg
 
 # todo: support magic values like pkg.__PATH__ for read access to pkg dir (i.e. for config, data files).
 #   hmm; do those files get bundled in a 'deploy package'? (what is a deploy package). how/where?
 
-CFunction = collections.namedtuple('CFunction', 'name type args')
+class ImexImportError(StandardError): pass
+class UnkSymbolError(ImexImportError): pass
+class AmbiguityError(ImexImportError): pass
+
+class ExportObject(object): "base class"
+
+# yeah, no chance this will ever be complete; the compiler needs to tell us about this. or just create a reasonable union and let the compiler complain.
+BUILTIN_TYPES = {'int', 'char', '*', 'float'}
+
+class CFunction(ExportObject):
+  "function declaration"
+  def __init__(self, name, type, args):
+    self.name, self.type, self.args = name, type, args
+  
+  @property
+  def types(self):
+    "return list of unique types used by the function declaration"
+    return set(sum([self.type]+[types for types, arg in self.args], []))
+
+  @property
+  def cline(self):
+    "return templated ({prefix} and {name} are in braces) C code to patch in somewhere"
+    # todo: types need to be renamed too if not builtin
+    return '{prefix}%s {name}(%s);' % (
+      ' '.join(self.type),
+      ', '.join('%s %s' % (' '.join(types), arg) for types, arg in self.args)
+    )
 
 def scrape_c(path):
   "return objects of interest from top level of C file. for now that's: typedef, function declaration, variables"
@@ -35,14 +62,52 @@ class Imexer:
     self.package_dir = os.path.split(package)[0]
     self.jpack = json.load(open(package))
 
-  def find(self, symbols):
+  def all_symbols(self):
+    "helper for self.find(). returns a bunch of dictionaries"
+    name_usage = collections.defaultdict(list)
+    full_lookups = {}
+    direct_lookups = {}
     for dirname, subdirs, files in os.walk(self.package_dir):
+      if any(pkg.util.hidden_dir(d) for d in dirname.split('/')): # skip hidden directories
+        # warning: splitting by '/' not platform-safe
+        continue
       for fname in files:
         if not fname.lower().endswith('.c'): continue
-        print scrape_c(os.path.join(dirname, fname))
-        raise NotImplementedError
-    # this has to tralp & preprocess
-    raise NotImplementedError
+        fullpath = os.path.join(dirname, fname)
+        # warning below: platform-unsafe delimiter (and other horrors)
+        prefix = tuple(filter(None, os.path.split(fullpath[len(dirname):])[0].split('/')))
+        res = scrape_c(fullpath)
+        for name, value in res.items():
+          # note below: overwriting here results in an extra name_usage entry. (there's also the 'folder + object same name' case)
+          direct_lookups[(name,)] = value
+          full_lookups[prefix + (name,)] = value
+          name_usage[name].append(fullpath)
+    
+    return full_lookups, direct_lookups, name_usage
+
+  def find(self, symbols):
+    "return dict {symbol:list_of_c_lines}"
+    # todo: all_symbols should return a DAG of functions & types
+    full, direct, name2file = self.all_symbols()
+    lines = {}
+    for symbol in symbols:
+      relative_symbol = symbol[1:] # strip out the package name. does this need to be checked? depends on source.
+      decl = None
+      if relative_symbol in full:
+        decl = full[relative_symbol]
+      elif relative_symbol in direct:
+        if len(name2file[relative_symbol[-1]]) > 1:
+          raise AmbiguityError(self.package_dir, symbol, name2file[relative_symbol])
+        decl = direct[relative_symbol]
+      else:
+        raise UnkSymbolError(self.package_dir, symbol)
+      if decl is None:
+        # ug.
+        raise RuntimeError('local "decl" was never set')
+      if set(decl.types) - BUILTIN_TYPES:
+        raise NotImplementedError('todo: include non-builtin types', set(decl.types) - BUILTIN_TYPES)
+      lines[symbol] = decl.cline
+    return lines
 
 def collect_symbols(package):
   build_order = depgraph.build_order(depgraph.deps(package))
@@ -58,5 +123,5 @@ def main():
 
   symbols = [tuple(sym.split('.')) for sym in args.symbols.split(',')]
   imexer = Imexer(args.package)
-  imexer.find(symbols)
-  raise NotImplementedError
+  for sym, cline in imexer.find(symbols).items():
+    print sym, cline
